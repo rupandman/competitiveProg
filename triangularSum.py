bb4314de8564524a2a98547d1bfaa3a1b9b19198
rupandman <61181030+rupandman@users.noreply.github.com>
2022-04-03 23:37:14 +0530

    Update triangularSumm.py

--- triangularSum.py
+++ triangularSum.py
@@ -1,9 +1,12 @@
 def triangularSum(ls):
+    
     if len(ls) == 1:
         return ls[0]
     while(len(ls)>1):
         ls = [(ls[i]+ls[i+1])%10 for i in range(len(ls)-1)]
     return ls[0]
+
+# Custom Input
 l = [1,2,3,4,5]
 print(triangularSum(l))
 
