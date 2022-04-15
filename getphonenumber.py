bf104877d823d7aad0378775c12d50c218d0bd56
rupandman <61181030+rupandman@users.noreply.github.com>
2022-04-15 10:29:18 +0530

    Update getphonenumber.py

--- getphonenumber.py
+++ getphonenumber.py
@@ -21,9 +21,3 @@ def getPhoneNumber(number):
 
 n = input("Enter a phone number: ")
 print(getPhoneNumber(n))
-
-# dic = {'0': "zero", '1':"one", '2':"two", '3':"three", '4':"four", '5':"five", '6':"six", '7':"seven", '8':"eight", '9':"nine"}
-# n = "567"
-# ls = [tuple(y) for x, y in groupby(n)]
-# for i in ls:
-#     print(dic[i[0]], end=" ")
